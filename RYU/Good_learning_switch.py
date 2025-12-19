87d9ea2bde544cc1eb1a18147d61734a6ce059e8
Manish Sarkar <sarkarmrigank28@gmail.com>
2025-12-19 20:21:43 +0530

    success on try 5

--- RYU/Good_learning_switch.py
+++ RYU/Good_learning_switch.py
@@ -1,6 +1,7 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet, ethernet
 
@@ -8,43 +9,65 @@ class PiSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.mac_table = {}
+        super(PiSwitch, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        # Install table-miss flow
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match, instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
+    def _packet_in_handler(self, ev):
         msg = ev.msg
-        dp = msg.datapath
-        ofp = dp.ofproto
-        parser = dp.ofproto_parser
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocol(ethernet.ethernet)
-        if eth.ethertype == 35020:  # ignore LLDP
-            return
-        src = eth.src
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
         dst = eth.dst
+        src = eth.src
 
-        dpid = dp.id
-        self.mac_table.setdefault(dpid, {})
-        self.mac_table[dpid][src] = in_port
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+        # Learn the MAC
+        self.mac_to_port[dpid][src] = in_port
 
-        if dst in self.mac_table[dpid]:
-            out_port = self.mac_table[dpid][dst]
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
         else:
-            out_port = ofp.OFPP_ALL  # FOR Pi, OFPP_FLOOD often fails
+            out_port = ofproto.OFPP_FLOOD
 
         actions = [parser.OFPActionOutput(out_port)]
 
-        # install flow if destination known
-        if out_port != ofp.OFPP_ALL:
-            match = parser.OFPMatch(eth_src=src, eth_dst=dst)
-            inst = [parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions)]
-            mod = parser.OFPFlowMod(datapath=dp, priority=1, match=match, instructions=inst)
-            dp.send_msg(mod)
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            self.add_flow(datapath, 1, match, actions, msg.buffer_id)
 
-        # send packet out
-        out = parser.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=msg.data)
-        dp.send_msg(out)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
